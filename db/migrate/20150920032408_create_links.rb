0fcac9f1d5f6980ab785970168b2b7c2601a0c9b
Jack Sung <magicsung@gmail.com>
2015-09-20 11:38:44 +0800

    create link table

--- db/migrate/20150920032408_create_links.rb
+++ db/migrate/20150920032408_create_links.rb
@@ -3,9 +3,9 @@ class CreateLinks < ActiveRecord::Migration
     create_table :links do |t|
       t.string :in_url
       t.text :out_url
-      t.integer :http_status
-
+      t.integer :http_status, :default => 301 # default to :permanent
       t.timestamps null: false
     end
+    add_index :links, :in_url
   end
 end
